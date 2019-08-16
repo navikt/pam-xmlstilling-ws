ab7d71410be85ccdcb42c7bcd41933730e2e99d9
Einar Bjerve <einar.bjerve@nav.no>
2019-08-16 12:52:26 +0200

    [Endret til log4j2 istedet for logback. Fjernet også unødvendige kall til logback-api'et. La til Logstash-encoding i prod-kode, og egen test-config for logging i test]

--- src/test/java/no/nav/xmlstilling/ws/dao/ValueObjectConverterTest.java
+++ src/test/java/no/nav/xmlstilling/ws/dao/ValueObjectConverterTest.java
@@ -1,30 +1,14 @@
 package no.nav.xmlstilling.ws.dao;
 
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
 import no.nav.xmlstilling.ws.common.vo.StillingBatchVO;
 import no.nav.xmlstilling.ws.entity.StillingBatchEntity;
-import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.LoggerFactory;
 
 import static no.nav.xmlstilling.ws.dao.ValueObjectConverter.getStillingBatchEntityForValueObject;
 import static no.nav.xmlstilling.ws.dao.ValueObjectConverter.getStillingBatchValueObject;
 import static org.junit.Assert.assertEquals;
 
 public class ValueObjectConverterTest {
-
-    private ListAppender<ILoggingEvent> loggingEventListAppender;
-
-    @Before
-    public void setUp() {
-        loggingEventListAppender = new ListAppender<ILoggingEvent>();
-        loggingEventListAppender.start();
-        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-        loggerContext.getLogger(Logger.ROOT_LOGGER_NAME).addAppender(loggingEventListAppender);
-    }
 
     @Test
     public void testGetStillingBatchValueObject() throws Exception {
